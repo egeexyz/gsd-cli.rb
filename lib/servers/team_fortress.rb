778271c2f33e79ad5e965d8bd818d3b728176290
egee-irl <brian@egee.io>
2019-03-02 03:03:16 -0800

    added property readers

--- lib/servers/team_fortress.rb
+++ lib/servers/team_fortress.rb
@@ -1,9 +1,14 @@
 # A module to produce resources for a Team Fortress 2 server
-module TeamFortress
-  def self.exec_start(file_path = "/tmp/gsd/tf2/server_files",
-                 log_path = "/tmp/gsd/tf2/server_files/tf/console.log",
-                 map = "ctf_2fort",
-                 players = 24)
+class TeamFortress
+  attr_reader :name
+  def initialize
+    @name = "tf2"
+  end
+
+  def exec_start(file_path = "/tmp/gsd/tf2/server_files",
+                      log_path = "/tmp/gsd/tf2/server_files/tf/console.log",
+                      map = "ctf_2fort",
+                      players = 24)
     "#{file_path}/srcds_run \
     -console \
     -game tf \

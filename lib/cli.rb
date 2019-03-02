3ac756350efdca465c6dd0f1ac0d074df4bd043a
egee-irl <brian@egee.io>
2019-03-02 03:03:47 -0800

    refactored logic out

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,11 @@ require "commander/import"
 require "./lib/deploy"
 require "./lib/daemonize"
 require "./lib/servers/team_fortress"
+require "./lib/servers"
+
+@game = {
+  "tf2" => TeamFortress.new
+}
 
 program :name, "gsc-cli"
 program :version, "0.0.1"
@@ -14,34 +19,8 @@ command :deploy do |c|
   c.example "description", "command example"
   c.option "--some-switch", "Some switch that does something"
   c.action do |args, options|
-    tf2_daemon = Daemonize.new("test",
-                               "tf2",
-                               "ubuntu",
-                               TeamFortress.exec_start())
-    Deploy.it(args.first, tf2_daemon.build())
-  end
-end
-
-command :build do |c|
-  c.syntax = "gsc-cli build [options]"
-  c.summary = "Build a systemd unit"
-  c.description = "Build Description"
-  c.example "description", "command example"
-  c.option "--some-switch", "Some switch that does something"
-  c.action do |args, options|
-    desc = "test desc"
-    game = "tf2"
-    user = "ubuntu"
-    exec_start = "/tmp/gsd/tf2/server_files/srcds_run \
-    -console \
-    -game tf \
-    +sv_pure 1 \
-    +map ctf_2fort \
-    +maxplayers 24 \
-    -condebug & \
-    /usr/bin/tail -f /tmp/gsd/tf2/server_files/tf/console.log"
-
-    daemonize = Daemonize.new(desc, game, user, exec_post, exec_start)
-    daemonize.it
+    daemon = Daemonize.new(@game[args.first])
+    daemon.build()
+    Deploy.it(daemon)
   end
 end

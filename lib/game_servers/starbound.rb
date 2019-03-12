86944e9e1a1493191dcb292cabd6247e39dd0fa1
egee-irl <brian@egee.io>
2019-03-11 21:29:58 -0700

    added

---
+++ lib/game_servers/starbound.rb
@@ -0,0 +1,13 @@
+# A module to produce resources for a Starbound server
+class Starbound
+  attr_reader :name, :app_id
+  def initialize
+    @name = "starbound"
+    @app_id = "533830"
+  end
+
+  def launch(install_path)
+    "cd #{install_path} &&
+    #{install_path}/linux/starbound_server"
+  end
+end

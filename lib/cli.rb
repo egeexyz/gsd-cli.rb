de70b2ed28ec24dbe70fc96dc8eaef0fdfcac00d
egee-irl <brian@egee.io>
2019-04-06 16:02:25 -0700

    bumped versions

--- lib/cli.rb
+++ lib/cli.rb
@@ -27,7 +27,7 @@ end
 @games = userdata
 
 program :name, "gsd-cli"
-program :version, "0.1.9"
+program :version, "0.1.11"
 program :description, "A cli tool to deploy & manage dedicated game servers on Linux"
 
 command :install do |c|

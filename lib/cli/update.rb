eadc61191cfcd6149c2010ed02a71a5152209a4f
egee-irl <brian@egee.io>
2019-07-26 03:18:04 -0700

    updated to match pattern used in install

--- lib/cli/update.rb
+++ lib/cli/update.rb
@@ -3,6 +3,7 @@
 require "commander/import"
 require "game_template"
 require "cli/game_hash"
+require "helpers"
 
 command :update do |c|
   c.syntax = "gsd update [args] [options]"
@@ -12,6 +13,21 @@ command :update do |c|
   c.option "--steamuser OPTIONAL", String, "Steam user account required to install certain games."
   c.option "--steampassword OPTIONAL", String, "Steam account password for installing certain games."
   c.action do |args, options|
-    GameTemplate.new(game_hash[args.first()], options.path).update(options.path)
+    game = game_hash[args.first()]
+    abort("Non-Steam games not supported at this time 😞") if (game.app_id.nil?)
+    update(game, options.path, options.steamuser, options.steampassword)
   end
+end
+
+# Update is functionally the same as install at this point because
+# Servers installed via steamcmd are simply "reinstalled" to update them
+# Non-steamcmd servers are not supported at this point (thats a 0.3.0 thing)
+def update(game, path, steam_user, steam_password)
+  puts "Beginning #{game.name} server update. This may take a while...".blue
+  system("rm -f #{"/tmp/#{game.name}.service"}")
+  system("rm -f /etc/systemd/system/#{game.name}.service")
+  GameTemplate.new(game)
+              .install(Helpers.get_install_path(path, game.name),
+                      Helpers.get_steamcmd_login(steam_user, steam_password))
+  puts "#{game.name} server update complete. Check the output for messages or warnings.".green
 end

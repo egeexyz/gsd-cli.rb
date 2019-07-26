f8fcf019352c73c1d45a69c0088c5f8dff29cf8d
egee-irl <brian@egee.io>
2019-07-26 03:17:35 -0700

    super fancy todos

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
+#TODOs:
+#1) Implement friendly-name attribute
+#2) Add tests for cli functions
+#3) Implement update for Minecraft (oof)
+#4) Implement ufw rules for game server objects
+
 require "commander/import"
 
 program :name, "gsd-cli"
-program :version, "0.1.37"
+program :version, "0.1.40"
 program :description, "A cli tool to deploy & manage dedicated game servers on Linux"
 
 require "cli/install"

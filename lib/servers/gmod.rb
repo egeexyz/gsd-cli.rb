85ac4fee69d80653f7a6d87956dfe0c554130c02
egee-irl <brian@egee.io>
2019-07-26 03:17:51 -0700

    friendly-name

--- lib/servers/gmod.rb
+++ lib/servers/gmod.rb
@@ -1,9 +1,10 @@
 # A module to produce resources for a Garry's Mod server
 class GarrysMod
-  attr_reader :name, :app_id
+  attr_reader :name, :app_id, :friendly_name
   def initialize
     @name = "gmod"
     @app_id = "4020"
+    @friendly_name = "Garry's Mod" #TODO: Add friendly name to all server objects
   end
 
   def launch(install_path, map = "gm_construct", players = 16, collection_id = "1270037458")

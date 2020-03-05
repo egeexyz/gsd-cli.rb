fa4260231d02f739f071531b74b1aa8d95514f09
egee-irl <brian@egee.io>
2020-03-05 13:40:10 -0800

    renamed builders to installers, added backend

--- src/supported-games.js
+++ src/supported-games.js
@@ -1,31 +1,36 @@
 const SupportedGames = [
   {
     name: 'scp',
-    type: 'steamcmd',
+    installer: 'steamcmd',
+    backend: 'scp',
     appId: 996560,
     launchParams: 'mono ./MultiAdmin.exe'
   },
   {
     name: 'tf2',
-    type: 'steamcmd',
+    installer: 'steamcmd',
+    backend: 'srcds',
     appId: 232250,
     launchParams: './srcds_run -console -game tf +map ctf_turbine +maxplayers 24 +sv_pure 1 -condebug'
   },
   {
     name: 'gmod',
-    type: 'steamcmd',
+    installer: 'steamcmd',
+    backend: 'srcds',
     appId: 4020,
     launchParams: './srcds_run -console -game garrysmod +map gm_construct +maxplayers 16 -condebug'
   },
   {
     name: 'rust',
-    type: 'steamcmd',
+    installer: 'steamcmd',
+    backend: 'unity',
     appId: 258550,
     launchParams: './RustDedicated +server.ip 0.0.0.0 +server.port 28015 +server.identity rust +rcon.web 1 +rcon.ip 0.0.0.0 +rcon.port 28016 +rcon.password _'
   },
   {
     name: 'sdtd',
-    type: 'steamcmd',
+    installer: 'steamcmd',
+    backend: 'unity',
     appId: 294420,
     launchParams: './7DaysToDieServer.x86_64 -quit -batchmode -nographics -dedicated'
   }
